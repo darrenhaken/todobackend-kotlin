25860db37d37244883b38466021c565378a4d7f7
Darren Haken <darrenhaken@gmail.com>
2017-09-23 07:33:32 +0100

    Add spring boot + kotlin

---
+++ README.md
@@ -0,0 +1,7 @@
+Todo-Backend in Kotlin
+
+This is a simple implementation of the Todo-Backend API spec.
+
+The API can be hit directly at (http://todo-backend-kotlin.herokuapp.com/todos)
+
+This application uses Spring Boot and an embedded H2 database
---
+++ src/main/kotlin/com/haken/todobackendkotlin/TodobackendKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.haken.todobackendkotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TodobackendKotlinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TodobackendKotlinApplication::class.java, *args)
+}
